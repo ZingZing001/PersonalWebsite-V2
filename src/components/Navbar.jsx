ed5502596d44e304c4cf8d02c2739ba006863fff
Johnson <johnson@Johnsons-MacBook-Pro.local>
2026-01-30 14:00:42 +1300

    Change Blog nav link to scroll to BlogSection on home page
    - Blog link now uses #blog anchor instead of /blog route
    - Users access full blog page via 'Explore All Articles' button only

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const NavItems = [
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
-  { name: "Blog", href: "/blog" },
+  { name: "Blog", href: "#blog" },
   { name: "Contact", href: "#contact" },
 ];
 
